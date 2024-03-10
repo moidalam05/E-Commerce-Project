aa24eb1416dd8bc1ea031c3541e7d381f9e27bc7
moidalam05 <moidalam005@hotmail.com>
2024-03-10 11:53:31 +0530

    Add signup controller, asyncHandler, and CustomError classes

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,38 @@
+// Signup controller
+
+import User from '../models/user.schema.js';
+import asyncHandler from '../service/asyncHandler.js';
+import CustomError from '../utils/CustomError.js';
+
+const signUp = asyncHandler(async (req, res) => {
+	// get the data from the request body or user
+	const { name, email, password } = req.body;
+
+	// validate the data
+	if (!name || !email || !password) {
+		throw new CustomError('Please provide all the required fields', 400);
+	}
+
+	if (password.length < 8) {
+		throw new CustomError('Password should be at least 8 characters long', 400);
+	}
+
+	if (!email.includes('@')) {
+		throw new CustomError('Please provide a valid email', 400);
+	}
+
+	// check if user already exists
+	const existingUser = await User.findOne({ email });
+
+	if (existingUser) {
+		throw new CustomError('User already exists', 400);
+	}
+
+	// create the user
+	const user = await User.create({ name, email, password });
+	const token = user.getJWTtoken();
+	user.password = undefined;
+
+	// send the response to the user
+	res.status(200).json({ success: true, user, token });
+});
