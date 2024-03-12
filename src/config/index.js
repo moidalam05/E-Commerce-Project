85d4153f3148757bbc6fa7d36fab594b56745041
moidalam05 <moidalam005@hotmail.com>
2024-03-12 07:14:52 +0530

    Add AWS SDK and S3 configuration

--- src/config/index.js
+++ src/config/index.js
@@ -6,6 +6,10 @@ const config = {
 	MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost:27017/ecomm',
 	JWT_SECRET: process.env.JWT_SECRET || 'yoursecretkey',
 	JWT_EXPIRY: process.env.JWT_EXPIRY || '30d',
+	S3_ACCESS_KEY: process.env.S3_ACCESS_KEY,
+	S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY,
+	S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
+	S3_REGION: process.env.S3_REGION,
 };
 
 export default config;
