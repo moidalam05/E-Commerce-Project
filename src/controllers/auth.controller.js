4836b0c45c4364de4619ba206dd4630fa9ee6dc9
moidalam05 <moidalam005@hotmail.com>
2024-03-16 15:36:52 +0530

    Add forgotPassword and resetPassword functionality

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.schema.js';
 import asyncHandler from '../service/asyncHandler.js';
 import CustomError from '../utils/CustomError.js';
+import mailHelper from '../utils/mailHelper.js';
 
 export const cookieOptions = {
 	expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
@@ -130,4 +131,87 @@ export const getProfile = asyncHandler(async (req, res) => {
 		throw new CustomError('User not found', 404);
 	}
 	res.status(200).json({ success: true, user });
+});
+
+/*******************************************************
+ * @FORGOT_PASSWORD
+ * @route http://localhost:5000/api/v1/auth/getProfile
+ * @description forgot password of the user
+ * @returns {Object} - user
+ *******************************************************/
+
+export const forgotPassword = asyncHandler(async (req, res) => {
+	const { email } = req.body;
+	if (!email) {
+		throw new CustomError('Please provide email', 400);
+	}
+
+	const user = await User.findOne({ email });
+	if (!user) {
+		throw new CustomError('User not found', 404);
+	}
+
+	const resetToken = user.generateForgotPasswordToken();
+	await user.save({ validateBeforeSave: false });
+	const resetUrl = `${req.protocol}://${req.get(
+		'host'
+	)}/api/v1/auth/password/reset/${resetToken}`;
+
+	const message = `You recently requested to reset your password for your account. Click the link below to reset it: \n\n ${resetUrl}\n\nIf you did not request a password reset, please ignore this email or contact our support team.\n\nThank you,`;
+
+	try {
+		await mailHelper({
+			email: user.email,
+			subject: 'Reset Your Password',
+			message,
+		});
+	} catch (error) {
+		user.forgotPasswordToken = undefined;
+		user.ForgotPasswordExpiry = undefined;
+		await user.save({ validateBeforeSave: false });
+		throw new CustomError('Email could not be sent', 500);
+	}
+});
+
+/*******************************************************
+ * @RESET_PASSWORD
+ * @route http://localhost:5000/api/v1/auth/resetPassword/:resetToken
+ * @description reset password of the user
+ * @returns {Object} - user
+ *******************************************************/
+export const resetPassword = asyncHandler(async (req, res) => {
+	const { token: resetToken } = req.params;
+	const { password, confirmPassword } = req.body;
+
+	const resetPasswordToken = crypto
+		.createHash('sha256')
+		.update(resetToken)
+		.digest('hex');
+
+	const user = await User.findOne({
+		forgotPasswordToken: resetPasswordToken,
+		ForgotPasswordExpiry: { $gt: Date.now() },
+	});
+
+	if (!user) {
+		throw new CustomError('Password reset token is invalid or expired', 400);
+	}
+	// Check if passwords match
+	if (password !== confirmPassword) {
+		throw new CustomError('Passwords do not match', 400);
+	}
+	// Set new password and update user
+	user.password = password;
+	user.forgotPasswordToken = undefined;
+	user.ForgotPasswordExpiry = undefined;
+
+	await user.save();
+	const token = user.getJWTtoken();
+	res.cookie('token', token, cookieOptions);
+
+	res.status(200).json({
+		success: true,
+		message: 'Password reset successfully',
+		user,
+	});
 });
