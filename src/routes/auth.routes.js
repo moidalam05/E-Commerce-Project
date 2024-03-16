cfba593b0d933fa54f5cef7b6cb7a01b6230febf
moidalam05 <moidalam005@hotmail.com>
2024-03-16 16:39:32 +0530

    Add password reset functionality and update auth routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,22 @@
 import { Router } from 'express';
-import { getProfile, signIn, signOut, signUp } from '../controllers/auth.controller.js';
+import {
+	forgotPassword,
+	getProfile,
+	resetPassword,
+	signIn,
+	signOut,
+	signUp,
+} from '../controllers/auth.controller.js';
 import { isSignedIn } from '../middlewares/auth.middleware.js';
 const router = Router();
 
 router.post('/signup', signUp);
 router.post('/login', signIn);
 router.get('/logout', signOut);
-router.get('/profile',[isSignedIn],getProfile)
+
+router.post('/password/forgot', forgotPassword);
+router.post('/password/reset/:token', resetPassword);
+
+router.get('/profile', isSignedIn, getProfile);
 
 export default router;
